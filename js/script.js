75c13148eee194e5dc53709f2a33928995f58f82
nirbhay244 <kumarnirbhay844@gmail.com>
2023-09-10 11:21:56 +0530

    localStorage practice

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let start =  () => {
+    document.getElementsByClassName("fanBox").style.animationPlayState='running';
+    document.getElementsByClassName("fanBox").style.animationDuration='10s';
+
+    console.log("hello");
+}
+let one =  () => {
+    console.log("one");
+}
+let two =  () => {
+    console.log("two");
+}
+let three =  () => {
+    console.log("three");
+}
